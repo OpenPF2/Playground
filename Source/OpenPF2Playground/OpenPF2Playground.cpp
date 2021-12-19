404411346b0f71f9127fd442a91e34f77404abf3
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-12-19 14:37:05 -0500

    Update Header Licenses for Several Files
    These don't contain any code from Epic.

--- Source/OpenPF2Playground/OpenPF2Playground.cpp
+++ Source/OpenPF2Playground/OpenPF2Playground.cpp
@@ -1,6 +1,7 @@
-// Copyright 2021 Guy Elsmore-Paddock. All Rights Reserved.
-// Adapted from content that is Copyright Epic Games, Inc. (Third Person Sample).
-// Licensed only for use with Unreal Engine.
+// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "OpenPF2Playground.h"
 
--- Source/OpenPF2PlaygroundEditor.Target.cs
+++ Source/OpenPF2PlaygroundEditor.Target.cs
@@ -1,7 +1,9 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class OpenPF2PlaygroundEditorTarget : TargetRules
 {
