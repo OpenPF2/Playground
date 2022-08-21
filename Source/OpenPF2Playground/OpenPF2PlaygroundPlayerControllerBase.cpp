e6205084eeb921379e7ef592f7a2cbbce7feacd1
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-08-21 00:45:04 -0400

    [#21] [#22] [#23] WIP: Encounter UI and Character Control Overhaul
    This commit changes the Player State class to a new custom player state
    Blueprint that uses the new OpenPF2 base class to implement the
    PF2-specific Player State interface to provide the controllable
    characters.
    
    In addition, this refactors most of the UI to work with the new pawn
    control logic that should soon allow us to display multiple controllable
    characters to the player, and allow the player to toggle between them.
    
    This does not yet work in Encounters, but should work very soon.

---
+++ Source/OpenPF2Playground/OpenPF2PlaygroundPlayerControllerBase.cpp
@@ -0,0 +1,18 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2022, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "OpenPF2PlaygroundPlayerControllerBase.h"
+
+void AOpenPF2PlaygroundPlayerControllerBase::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	this->ControlledCharacterInputComponent =
+		NewObject<UInputComponent>(this, UInputComponent::StaticClass(), TEXT("PC_ControlledCharacterInput"));
+
+	this->ControlledCharacterInputComponent->RegisterComponent();
+
+	this->PushInputComponent(this->ControlledCharacterInputComponent);
+}
---
+++ Source/OpenPF2Playground/OpenPF2PlaygroundPlayerControllerBase.h
@@ -0,0 +1,31 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2022, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "PF2PlayerControllerBase.h"
+
+#include "OpenPF2PlaygroundPlayerControllerBase.generated.h"
+
+/**
+ * Base class for OpenPF2 Playground player controllers.
+ */
+UCLASS()
+class OPENPF2PLAYGROUND_API AOpenPF2PlaygroundPlayerControllerBase : public APF2PlayerControllerBase
+{
+	GENERATED_BODY()
+
+protected:
+	/**
+	 * An input component for directing player actions to whichever character is currently being controlled.
+	 */
+	UPROPERTY(BlueprintReadOnly)
+	UInputComponent* ControlledCharacterInputComponent;
+
+	// =================================================================================================================
+	// Protected Methods - APlayerController Overrides
+	// =================================================================================================================
+	virtual void SetupInputComponent() override;
+};
